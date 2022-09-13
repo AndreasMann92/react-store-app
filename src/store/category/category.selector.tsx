fd5c99d8ceef49f731140f2087fc1f538fc550eb
AndreasMann92 <andreasmann92@gmail.com>
2022-09-13 15:43:23 +0200

    implemented memoization for category selector

--- src/store/category/category.selector.tsx
+++ src/store/category/category.selector.tsx
@@ -1,9 +1,25 @@
+import { createSelector } from "reselect";
 import { RootState } from "../root-reducer";
-import { CategoriesMap } from "./category.types";
+import { CategoriesMap, CategoryArray, CategoryState } from "./category.types";
 
-export const selectCategoriesMap = (state: RootState) =>
-  state.categories?.categories?.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {} as CategoriesMap);
+const selectCategoryReducer = (state: RootState) => {
+  return state.categories;
+};
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  ({ categories }: CategoryState) => {
+    return categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories: CategoryArray) => {
+    return categories?.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {} as CategoriesMap);
+  }
+);
